c5ff98180ee32bf88800cbe6de491e343d6dae32
margaritaAZ <45636462+margaritaAZ@users.noreply.github.com>
2021-06-05 13:32:40 +0300

    feat: implement tasks 1-3 solution

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,33 @@ import Foundation
 
 final class FillWithColor {
     
+    func changePixelColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int, _ oldColor: Int) -> [[Int]]{
+        guard (row >= 0 && row < image.count && column >= 0 && column < (image.first?.count ?? 0) && newColor != oldColor) else {
+            return image
+        }
+        
+        var newImage = image
+        if  oldColor == image[row][column] {
+            newImage[row][column] = newColor
+            newImage = changePixelColor(newImage, row-1, column, newColor, oldColor)
+            newImage = changePixelColor(newImage, row+1, column, newColor, oldColor)
+            newImage = changePixelColor(newImage, row, column-1, newColor, oldColor)
+            newImage = changePixelColor(newImage, row, column+1, newColor, oldColor)
+        }
+        return newImage
+    }
+    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        let m = image.count
+        let n = image.first?.count ?? 0
+        
+        guard  (m <= 50 && m >= 1 && n <= 50 && n >= 1 &&
+                newColor >= 0 && newColor < 65536 &&
+                row >= 0 && row < m && column >= 0 && column < n) else {
+            return image
+        }
+        
+        return changePixelColor(image, row, column, newColor, image[row][column])
+        
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,177 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var stationUsers: [User]?
+    var callsList: [Call]?
+    
+}
 
 extension CallStation: Station {
     func users() -> [User] {
-        []
+        return self.stationUsers ?? []
     }
     
     func add(user: User) {
 
+        guard !(self.stationUsers?.contains(user) ?? false) else{
+            return
+        }
+        
+        if self.stationUsers != nil {
+            self.stationUsers?.append(user)
+        } else {
+            self.stationUsers = [user]
+        }
     }
     
     func remove(user: User) {
-
+        guard let users = stationUsers else {
+            return
+        }
+        var userIndex: Int?
+        for (index,stationUser) in users.enumerated() {
+            if stationUser.id == user.id {
+                userIndex = index
+            }
+        }
+        
+        if let index = userIndex {
+            stationUsers?.remove(at: index)
+        }
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        switch action {
+        case .start(let user1, let user2):
+            guard let users = stationUsers else {
+                return nil
+            }
+            guard users.contains(user1) else {
+                return nil
+            }
+            guard users.contains(user2) else {
+                let call = Call(id: UUID(), incomingUser: user1, outgoingUser: user2, status: .ended(reason: .error))
+                if callsList != nil {
+                    callsList?.append(call)
+                }
+                callsList = [call]
+                return call.id
+            }
+            
+            if let calls = callsList {
+                for call in calls{
+                    // добавить условие наоборот
+                    if (call.outgoingUser.id == user2.id && call.incomingUser.id != user1.id && call.status == .talk) {
+                        let call = Call(id: UUID(), incomingUser: user1, outgoingUser: user2, status: .ended(reason: .userBusy))
+                        callsList?.append(call)
+                        return call.id
+                    }
+                    let call = Call(id: UUID(), incomingUser: user1, outgoingUser: user2, status: .calling)
+                    callsList?.append(call)
+                    return call.id
+                }
+            }
+            let call = Call(id: UUID(), incomingUser: user1, outgoingUser: user2, status: .calling)
+            self.callsList = [call]
+            return call.id
+            
+        case .answer(let user2):
+            guard let calls = callsList else {
+                return nil
+            }
+            
+           // var callIndex: Int?
+            var callAnswer: Call?
+            for (index, call) in calls.enumerated() {
+                if (call.outgoingUser.id == user2.id && call.status == .calling) {
+                   // callIndex = index
+                    if (stationUsers?.contains(user2) ?? false) {
+                        callAnswer = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .talk)
+                        self.callsList?.remove(at: index)
+                        self.callsList?.insert(callAnswer!, at: index)
+                        return callAnswer?.id
+                    } else {
+                        callAnswer = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .error))
+                        self.callsList?.remove(at: index)
+                        self.callsList?.insert(callAnswer!, at: index)
+                        return nil
+                    }
+                }
+            }
+            return nil
+            
+        case .end(let user):
+            guard let calls = callsList else {
+                return nil
+            }
+            
+            var callIndex: Int?
+            var callTalking: Call?
+            for (index, call) in calls.enumerated() {
+                if ((call.outgoingUser.id == user.id || call.incomingUser.id == user.id) &&
+                        call.status == .talk) {
+                    callIndex = index
+                    callTalking = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .end))
+                    break
+                }
+                if (call.outgoingUser.id == user.id && call.status == .calling) {
+                    callIndex = index
+                    callTalking = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .cancel))
+                    break
+                }
+            }
+            if callIndex != nil {
+                self.callsList?.remove(at: callIndex!)
+                self.callsList?.insert(callTalking!, at: callIndex!)
+                return callTalking?.id
+            }
+            return nil
+        }
     }
     
     func calls() -> [Call] {
-        []
+        return self.callsList ?? []
     }
     
     func calls(user: User) -> [Call] {
-        []
+        guard let calls = callsList else {
+            return []
+        }
+        
+        var userCalls: [Call] = []
+        for call in calls {
+            if call.outgoingUser.id == user.id || call.incomingUser.id == user.id {
+                userCalls.append(call)
+            }
+        }
+        return userCalls
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        guard let calls = callsList else {
+            return nil
+        }
+        
+        for call in calls {
+            if call.id == id {
+                return call
+            }
+        }
+        return nil
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        guard let calls = callsList else {
+            return nil
+        }
+        
+        for call in calls {
+            if ((call.incomingUser.id == user.id || call.outgoingUser.id == user.id) && call.status == .calling) {
+                return call
+            }
+        }
+        
+        return nil
     }
 }
+
